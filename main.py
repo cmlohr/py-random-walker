7d6e8f9f840de97215d925f867c7c3e4066be4c0
Colleen Lohr <cmlohr@outlook.com>
2021-12-30 05:25:46 -0600

    added random colors
    Also changed width and speed of the walker for a cleaner look

--- main.py
+++ main.py
@@ -2,52 +2,17 @@ from turtle import Turtle, Screen
 import random
 
 tom = Turtle()
-tom.shape("classic")
-tom.color("blue")
-tom.speed("fastest")
-tom.width(14)
+# Screen().bgcolor("black")
+Screen().bgcolor("white")
+direction = [0, 90, 180, 270]
+tom.hideturtle()
+tom.speed("fast")
+tom.width(3)
 
-
-def change_color():
-    red = random.random()
-    green = random.random()
-    blue = random.random()
-    tom.color(red, green, blue)
-
-
-# random walk
-def move_right():
-    change_color()
-    tom.right(90)
-    tom.forward(30)
-
-
-def move_left():
-    change_color()
-    tom.left(90)
-    tom.forward(30)
-
-
-def move_forward():
-    change_color()
-    tom.forward(30)
-
-
-def move_backward():
-    change_color()
-    tom.backward(30)
-
-
-walking = [move_left, move_right, move_forward, move_backward]
-
-
-def random_walk():
-    random.choice(walking)()
-
-
-for _ in range(500):
-    random_walk()
-
+for _ in range(1200):
+    tom.color(random.random(), random.random(), random.random())
+    tom.forward(25)
+    tom.setheading(random.choice(direction))
 
 screen = Screen()
 screen.exitonclick()
